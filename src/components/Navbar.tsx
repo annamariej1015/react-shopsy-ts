40f72fa6125a3196a735e2f8c1e21c4143f8a56b
Anna <annamariej1015@gmail.com>
2021-04-26 08:50:39 -0700

    feature: navbar error, imported bootstrap

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Navbar =()=>{
+const Navbar = () => {
     return(
-        <div id='navbar'>
-            <div className="row text-center">
-                <div className="col">
-                    <h2>Navbar</h2>
+        <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+            <Link to='/' className='navbar-brand'>Shopsy</Link>
+            
+            <div className="collapse navbar-collapse">
+                <div className="navbar-nav">
+                    <Link to='/' className='nav-link'>Home</Link>
                 </div>
             </div>
-        </div>
-    )
+
+            <div className="sh-cart-icon">
+                <div className="navbar-nav">
+                    <Link to='/cart' className="nav-link">
+                        Cart
+                    </Link>
+                </div>
+            </div>
+    </nav>
+    );
 };
 
 export default Navbar;
--- src/routers/AppRoutes.tsx
+++ src/routers/AppRoutes.tsx
@@ -1,19 +1,24 @@
 import { Switch, Route } from 'react-router-dom';
-
+//Pages
 import HomePage from '../pages/HomePage';
 import CartPage from '../pages/CartPage';
 import CheckoutPage from '../pages/CheckoutPage';
 import ProductPage from '../pages/ProductPage';
+//Components
+import Navbar from '../components/Navbar';
 
 const AppRoutes = () => {
     return(
         <div>
-            <Switch>
-                <Route path='/' exact component={HomePage}/>
-                <Route path='/products/:productId' exact component={ProductPage}/>
-                <Route path='/cart' component={CartPage}/>
-                <Route path='/checkout' component={CheckoutPage}/>
-            </Switch>
+            <Navbar/>
+            <div className="container">
+                <Switch>
+                    <Route path='/' exact component={HomePage}/>
+                    <Route path='/products/:productId' exact component={ProductPage}/>
+                    <Route path='/cart' component={CartPage}/>
+                    <Route path='/checkout' component={CheckoutPage}/>
+                </Switch>
+            </div>
         </div>
     );
 };
